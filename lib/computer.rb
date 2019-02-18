b275d5dcf21bc12fdc8ad7bfacf45b3587e1acdb
Chris Wegscheid <cwegscheid08@outlook.com>
2019-02-18 16:34:02 -0500

    Added computer name and color assignment.

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,7 +1,7 @@
 class Computer
 	attr_accessor :name, :side_color
 
-	def initialize(name, side_color)
+	def initialize(name = "computer", side_color)
 		@name = name
 		# @game_piece = game_piece
 		@side_color = side_color
